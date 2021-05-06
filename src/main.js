26be1180f2ba47077d7adb69122516b48c3fd20c
StormZZ001 <976183601@qq.com>
2021-05-06 22:11:06 +0800

    1.使用移动端REM适配lib-flexible动态设置REM基准值(html标签的字体大小)

--- src/main.js
+++ src/main.js
@@ -4,7 +4,9 @@ import router from './router'
 import store from './store'
 
 Vue.config.productionTip = false
-
+//引入全局ui组件库-vantui
+import '@/plugins/vant'
+import 'amfe-flexible'
 new Vue({
   router,
   store,
